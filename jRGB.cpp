2ac2cc89f0eaa7f625bd7bc8d554f567bb0090cc
jKill709 <jeremykillinger@gmail.com>
2026-06-05 15:09:53 -0400

    Initial commit

---
+++ jRGB.cpp
@@ -0,0 +1,55 @@
+#include "Arduino.h"
+#include "jRGB.h"
+
+void jRGB::setPins(int redPin, int greenPin, int bluePin, bool isCommonAnode, int red, int green, int blue){
+	_redPin = redPin;
+	_greenPin = greenPin;
+	_bluePin = bluePin;
+	_isCommonAnode = isCommonAnode;
+
+	pinMode(_redPin, OUTPUT);
+	pinMode(_greenPin, OUTPUT);
+	pinMode(_bluePin, OUTPUT);
+
+	setLED(red, green, blue);
+}
+
+void jRGB::setLED(int red, int green, int blue){
+	_red = red;
+	_green = green;
+	_blue = blue;
+
+	if (isCommonAnode) {
+		analogWrite(ledRed, 255 - redValue);
+		analogWrite(ledGreen, 255 - greenValue);
+		analogWrite(ledBlue, 255 - blueValue);
+	} else {
+		analogWrite(ledRed, redValue);
+		analogWrite(ledGreen, greenValue);
+		analogWrite(ledBlue, blueValue);
+	}
+}
+
+void jRGB::fadeLED(int redStart, int greenStart, int blueStart, int redEnd, int greenEnd, int blueEnd, int timePeriod){
+	int t;
+
+	for (t = 0; t < 32; t++) {
+		setLED(redStart + (t*((redEnd - redStart) / 32)), greenStart + (t*((greenEnd - greenStart) / 32)), blueStart + (t*((blueEnd - blueStart) / 32)));
+
+		delay(timeSpan / 32);
+	}
+
+	setLED(redEnd, greenEnd, blueEnd);
+}
+
+void jRGB::flashLEDred(int numberOfFlashes){
+	int i;
+
+	setLED(0, 0, 0);
+	for (i = 0; i < numberOfFlashes; i++){
+		setLED(255, 0, 0);
+		delay (250);
+		setLED(0, 0, 0);
+		delay (250);
+	}
+}
---
+++ jRGB.h
@@ -0,0 +1,38 @@
+#include <arduino.h>
+
+#ifndef jRGB_h
+#define jRGB_h
+
+class jRGB {
+	public:
+		jRGB(int redPin, int greenPin, int bluePin, bool isCommonAnode) {
+			setPins(redPin, greenPin, bluePin, isCommonAnode, 0, 0, 0);
+		};
+		jRGB(int redPin, int greenPin, int bluePin, int red, int green, int blue)
+			setPins(redPin, greenPin, bluePin, isCommonAnode, red, green, blue);
+		};
+		int getRed() {
+			return _redComp;
+		};
+		int getGreen() {
+			return _greenComp;
+		};
+		int getBlue() {
+			return _blueComp;
+		};
+		void setLED(int red, int green, int blue);
+		void fadeLED(int redStart, int greenStart, int blueStart, int redEnd, int greenEnd, int blueEnd, int timePeriod);
+		void flashLEDred(int numberOfFlashes);
+	private:
+		int _redPin;
+		int _greenPin;
+		int _bluePin;
+		bool _isCommonAnode = false;
+		int _redComp = 0;
+		int _greenComp = 0;
+		int _blueComp = 0;
+
+		void setPins(int redPin, int greenPin, int bluePin, bool isCommonAnode, int red, int green, int blue);
+};
+
+#endif
